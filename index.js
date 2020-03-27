94fb84ed8639fafbfc87077271e63822d85a81e6
emafazillah <gatsu2811@gmail.com>
2020-03-27 23:10:24 +0800

    Parse remote CSV file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-'use strict'
+const {StringStream} = require("scramjet");
+const request = require("request");
 
-const fs = require('fs');
-
-fs.readFile('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-26-2020.csv', 'utf8', function (err, data) {
-  var dataArray = data.split(/\r?\n/);
-  console.log(dataArray);
-});
+request.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-26-2020.csv")   // fetch csv
+    .pipe(new StringStream())                       // pass to stream
+    .CSVParse()                                   // parse into objects
+    .consume(object => console.log("Row:", object))  // do whatever you like with the objects
+    .then(() => console.log("all done"));
