ecca7e0471e97210bba1e4daac9ecf08ebf1a310
emafazillah <gatsu2811@gmail.com>
2020-03-28 23:56:03 +0800

    Update bot message

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
+require('dotenv').config();
 const { StringStream } = require('scramjet');
 const request = require('request');
 const Telegram = require('node-telegram-bot-api');
 const bot = new Telegram(process.env.TELEGRAM_TOKEN);
 
+const URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/';
+const CSV = '.csv';
+
 const today = new Date();
-const yesterdayDate = today.getDate() - 1;
-const yesterdayMonth = today.getMonth() + 1;
-let yesterday = '';
-if (yesterdayMonth < 10) {
-    yesterday = '0' + yesterdayMonth + '-' + yesterdayDate + '-' + today.getFullYear();
+const yesterday = new Date(today);
+yesterday.setDate(yesterday.getDate() - 1);
+let formattedYesterdayDate = '';
+const getMonth = yesterday.getMonth() + 1;
+if (getMonth < 10) {
+    formattedYesterdayDate = '0' + getMonth + '-' + yesterday.getDate() + '-' + yesterday.getFullYear();
 } else {
-    yesterday = yesterdayMonth + '-' + yesterdayDate + '-' + today.getFullYear();
+    formattedYesterdayDate = getMonth + '-' + yesterday.getDate() + '-' + yesterday.getFullYear();
 }
 
 const generateMessage = object => 
-    `Malaysia COVID-19 Updates; 
+    `Malaysia COVID-19 Updates as ${yesterday.toDateString()}; 
     Confirmed: ${object.Confirmed}. 
     Deaths: ${object.Deaths}. 
     Recovered: ${object.Recovered}. 
     Active: ${object.Active}.`;
 
 // TODO: Get country region from configuration
-const url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/' + yesterday + '.csv';
-request.get(url)
+request.get(URL + formattedYesterdayDate + CSV)
     .pipe(new StringStream())
     .CSVParse({ skipEmptyLines: true, header: true })
     .filter(object => (object.Country_Region === 'Malaysia'))
     .map(async(object) => {
         const message = generateMessage(object);
-        // console.log('message: ', message);
         bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
     });
