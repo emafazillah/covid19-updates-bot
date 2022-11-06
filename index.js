adaa5af3d92ec626fc616ac1583a4af00350351f
emafazillah <gatsu2811@gmail.com>
2022-11-07 02:03:32 +0800

    Add today's new case, recovered and death.

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ import { StringStream } from 'scramjet';
 import { get } from 'request';
 import Telegram from 'node-telegram-bot-api';
 const bot = new Telegram(process.env.TELEGRAM_TOKEN);
-import mysql from 'mysql';
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: '',
-    password: '',
-    database: ''
-});
 
 const URL = process.env.URL;
 const CSV = process.env.CSV;
@@ -19,20 +12,29 @@ const COUNTRY = process.env.COUNTRY;
 try {
     const today = new Date();
     const yesterday = new Date(today);
+    const dayBeforeYesterday = new Date(today);
     yesterday.setDate(yesterday.getDate() - 1);
+    dayBeforeYesterday.setDate(dayBeforeYesterday.getDate() - 2);
     let formattedYesterdayDate = '';
+    let formattedDayBeforeYesterdayDate = '';
     const getMonth = yesterday.getMonth() + 1;
-    if (getMonth < 10) {
+    const getMonthBefore = dayBeforeYesterday.getMonth() + 1;
+    if (getMonth < 10 || getMonthBefore < 10) {
         formattedYesterdayDate = '0' + getMonth + '-' + yesterday.getDate() + '-' + yesterday.getFullYear();
+        formattedDayBeforeYesterdayDate = '0' + getMonthBefore + '-' + dayBeforeYesterday.getDate() + '-' + dayBeforeYesterday.getFullYear();
     } else {
         formattedYesterdayDate = getMonth + '-' + yesterday.getDate() + '-' + yesterday.getFullYear();
+        formattedDayBeforeYesterdayDate = getMonthBefore + '-' + dayBeforeYesterday.getDate() + '-' + dayBeforeYesterday.getFullYear();
     }
-    
-    const generateMessage = (country, totalConfirmed, totalDeaths, totalRecovered, totalActive) => 
-        `${country} COVID-19 Update as ${yesterday.toDateString()}; 
-        Total Confirmed: ${totalConfirmed}. 
-        Total Deaths: ${totalDeaths}. 
-        Total Recovered: ${totalRecovered}. 
+
+    const generateMessage = (country, newcase, deaths, recovered, totalConfirmed, totalDeaths, totalRecovered, totalActive) =>
+        `${country} COVID-19 Update as ${yesterday.toDateString()};
+        New Case: ${newcase}.
+        Deaths: ${deaths}.
+        Recovered: ${recovered}.
+        Total Confirmed: ${totalConfirmed}.
+        Total Deaths: ${totalDeaths}.
+        Total Recovered: ${totalRecovered}.
         Total Active: ${totalActive}.`;
 
     async function getResult(country) {
@@ -42,24 +44,55 @@ try {
                 .CSVParse({ delimiter: ',', skipEmptyLines: true, header: true })
                 .filter(data => (data.Country_Region === country))
                 .consume(data => arr.push(data));
+        await get(URL + formattedDayBeforeYesterdayDate + CSV)
+                .pipe(new StringStream())
+                .CSVParse({ delimiter: ',', skipEmptyLines: true, header: true })
+                .filter(data => (data.Country_Region === country))
+                .consume(data => arr.push(data));
         return arr;
     }
-    
+
     getResult(COUNTRY)
         .then(result => {
+            let newcase = 0;
+            let deaths = 0;
+            let recovered = 0;
             let totalConfirmed = 0;
             let totalDeaths = 0;
             let totalRecovered = 0;
             let totalActive = 0;
+            let curr = '';
+            let prev = '';
 
             result.forEach(element => {
-                totalConfirmed = totalConfirmed + parseInt(element.Confirmed);
-                totalDeaths = totalDeaths + parseInt(element.Deaths);
-                totalRecovered = totalRecovered + parseInt(element.Recovered);
-                totalActive = totalActive + parseInt(element.Active);
+                if (curr === '')
+                {
+                    totalConfirmed = parseInt(element.Confirmed);
+                    totalDeaths = parseInt(element.Deaths);
+                    totalRecovered = parseInt(element.Recovered);
+                    totalActive = parseInt(element.Active);
+                }
+                else
+                {
+                    prev = curr;
+                }
+                curr = element.Last_Update.replace(' ', 'T') + 'Z';
+                newcase = Math.abs(newcase - parseInt(element.Confirmed));
+                deaths = Math.abs(deaths - parseInt(element.Deaths));
+                recovered = Math.abs(recovered - parseInt(element.Recovered));
+                if (prev !== '')
+                {
+                    if (new Date(curr) > new Date(prev))
+                    {
+                        totalConfirmed = parseInt(element.Confirmed);
+                        totalDeaths = parseInt(element.Deaths);
+                        totalRecovered = parseInt(element.Recovered);
+                        totalActive = parseInt(element.Active);
+                    }
+                }
             });
-            
-            const message = generateMessage(COUNTRY, totalConfirmed, totalDeaths, totalRecovered, totalActive);
+
+            const message = generateMessage(COUNTRY, newcase, deaths, recovered, totalConfirmed, totalDeaths, totalRecovered, totalActive);
             bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
         });
 
